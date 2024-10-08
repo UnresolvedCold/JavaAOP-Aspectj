24b14fdcb8d7a55e37b8939167d4588feed271ae
Shubham Kumar <unresolved.shubham@gmail.com>
2024-10-09 03:00:08 +0530

    Run the class using mvn exec:java -Dexec.mainClass="codes.shubham.aop.play.Play"

---
+++ src/main/java/codes/shubham/aop/play/Play.java
@@ -0,0 +1,11 @@
+package codes.shubham.aop.play;
+
+public class Play {
+  public static void helloWorld() {
+    System.out.println("Hello World");
+  }
+
+  public static void main(String[] args) {
+    helloWorld();
+  }
+}
---
+++ src/main/java/codes/shubham/aop/play/PlayAspect.java
@@ -0,0 +1,24 @@
+package codes.shubham.aop.play;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class PlayAspect {
+  @Pointcut("execution(* main(..))")
+  public void defineEntryPoint() {
+  }
+
+  @Before("defineEntryPoint()")
+  public void aaa(JoinPoint joinPoint) {
+    System.out.println("aspect before");
+  }
+
+  @After("defineEntryPoint()")
+  public void bbb(JoinPoint joinPoint) {
+    System.out.println("aspect after");
+  }
+}
